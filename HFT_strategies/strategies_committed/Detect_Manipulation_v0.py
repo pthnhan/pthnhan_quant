e204538ecb96f0c6ddf1d78dc13d75a470dbc58d
pthnhan <nhanmath97@gmail.com>
2020-09-30 14:49:55 +0700

    update detect manipulation

--- HFT_strategies/strategies_committed/Detect_Manipulation_v0.py
+++ HFT_strategies/strategies_committed/Detect_Manipulation_v0.py
@@ -79,7 +79,7 @@ class ManipulationDetection():
         #                     break
         #     count+=1
         #     print(f"count: {start+count}")
-
+        detect = ""
         vol_buy_cancelled_at_t = vol_buy_cancelled.iloc[-2]
         vol_buy_matched = MD.getPbidVbidmatched(start, end-1)[1]
         if len(vol_buy_matched) >0:
@@ -88,16 +88,13 @@ class ManipulationDetection():
                 if len(MD.getPsellVsellMatched(start, end-1)[0]) > 0:
                     price_sell_max_matched_t = max(MD.getPsellVsellMatched(start, end-1)[0])
                     price_sell_min_matched_t_add_1 = min(MD.getPsellVsellMatched(start, end)[0])
-                    print(f"Price_sell_max_matched_t: {price_sell_max_matched_t}")
-                    print(f"Price_sell_min_matched_t + 1: {price_sell_min_matched_t_add_1}")
                     if price_sell_max_matched_t-price_sell_min_matched_t_add_1 > self.params["threshold2"]:
-                        print("Detected dumping state!")
+                        detect = "dump"
                         price_bid_max_matched_t_minus_1 = max(MD.getPbidVbidmatched(start, end-2)[0])
                         price_bid_min_matched_t_minus_4 = min(MD.getPbidVbidmatched(start, end-5)[0])
                         if price_bid_max_matched_t_minus_1 - price_bid_min_matched_t_minus_4 > self.params["threshold3"]:
-                            print("Detected pumping state!")
-                            return 1
-        return 0
+                            detect = "pump and dump"
+        return detect
 
 
     # Check spoofing
@@ -122,21 +119,41 @@ class ManipulationDetection():
 
 if __name__ == '__main__':
     df = pd.read_csv('/home/thanhnhan/Desktop/pthnhan_quant/HFT_strategies/HFT/VN30F1M.csv', index_col=0, parse_dates=['Date'])
-    # MD = ManipulationDetection(df)
-    # i=200
-    # pump_and_dump = []
-    # while i <8000:
-    #     detect = MD.detect_pump_and_dump(i-200, i)
-    #     if detect ==1:
-    #         print(f"Detected pump-and-dump state! {i}")
-    #         pump_and_dump.append(i)
-    #     i+=1
+    MD = ManipulationDetection(df)
+
+    i=200
+    s = 0
+    dump = []
+    pump_and_dump = []
+    while i <4650:
+        bid_price = df[i:i+1]["bid_price"]
+        print(f"bid_price: {bid_price}")
+        ask_price = df[i:i+1]["ask_price"]
+        print(f"ask_price: {ask_price}")
+        detect = MD.detect_pump_and_dump(i - 200, i)
+        if detect == "dump":
+            dump.append(i)
+            print(f"Detected dumping state at the following times!")
+        if detect == "pump and dump":
+            pump_and_dump.append(i)
+            print(f"Detected pumping and dumping state at the following times!")
+        if len(dump) > 0:
+            pump_and_dump = []
+            if len(dump) == 3:
+                s += bid_price
+                print(f"s: {s}")
+        if len(pump_and_dump) > 0:
+            dump = []
+            if len(pump_and_dump) == 3:
+                s -= ask_price
+                print(f"s: {s}")
+        i+=1
     #
     # print(pump_and_dump)
     # plt.plot(df[0:8000]['ask_price'])
     # plt.show()
 
     # print(MD.detect_bump_and_dump(232,432))
-    print(tabulate(df[0:200], tablefmt='pipe', headers='keys'))
+    # print(tabulate(df[4650:4651], tablefmt='pipe', headers='keys'))
     # print(tabulate(df[200:555], tablefmt='pipe', headers='keys'))
     # print(tabulate(df[555:755], tablefmt='pipe', headers='keys'))
