645b875bbc39ce5454cb1939069ff0cd92315f61
pthnhan <nhanmath97@gmail.com>
2020-10-01 10:41:31 +0700

    update detect manipulation_v0

--- HFT_strategies/strategies_committed/Detect_Manipulation_v0.py
+++ HFT_strategies/strategies_committed/Detect_Manipulation_v0.py
@@ -55,7 +55,6 @@ class ManipulationDetection():
     #Check pump and dump
     def detect_pump_and_dump(self, start, end):
         df = self.df[start:end]
-        print(self)
         vol_buy_cancelled = df['cancelled_bid']
         # while count <len(df)-1:
         #     vol_buy_cancelled_at_t = vol_buy_cancelled.iloc[count]
@@ -118,42 +117,112 @@ class ManipulationDetection():
 
 
 if __name__ == '__main__':
-    df = pd.read_csv('/home/thanhnhan/Desktop/pthnhan_quant/HFT_strategies/HFT/VN30F1M.csv', index_col=0, parse_dates=['Date'])
+    df = pd.read_csv('/home/thanhnhan/Desktop/pthnhan_quant/HFT_strategies/strategies/VN30F1M.csv', index_col="Date", parse_dates=['Date'])
+    df = df[df.index.isnull() == False]
+    df_date = df[df.index.strftime("%Y-%m-%d").isin(["2020-09-29"])]
+    # print(df[-len(df_date):-3000])
+    index =[]
     MD = ManipulationDetection(df)
+    # l=-len(df_date)
+    # s = 0
+    # check = []
+    # dump = []
+    # pump_and_dump = []
+    # while l < 0:
+    #     print(l)
+    #     bid_price = df_date[l - 200:l]["bid_price"]
+    #     ask_price = df_date[l - 200:l]["ask_price"]
+    #     detect = MD.detect_pump_and_dump(l - 200, l)
+    #     if detect == "dump":
+    #         check.append("dump")
+    #         print(f"Detected dumping state at the following times!")
+    #         if check[-3:] == ["dump", "dump", "dump"] and abs(len(dump) + 1 - len(pump_and_dump) < 2):
+    #             bidprice = bid_price[-1]
+    #             print(f"bid_price: {bidprice}")
+    #             dump.append(bidprice)
+    #             print(f"dump : {dump}")
+    #             print(f"pump and dump: {pump_and_dump}")
+    #             s += bidprice
+    #             print(f"s: {s}")
+    #     if detect == "pump and dump":
+    #         check.append("pump and dump")
+    #         print(f"Detected pumping and dumping state at the following times!")
+    #         if check[-3:] == ["pump and dump", "pump and dump", "pump and dump"] and abs(
+    #                 len(pump_and_dump) + 1 - len(dump)) < 2:
+    #             askprice = ask_price[-1]
+    #             print(f"ask_price: {askprice}")
+    #             pump_and_dump.append(askprice)
+    #             print(f"dump : {dump}")
+    #             print(f"pump and dump: {pump_and_dump}")
+    #             s -= askprice
+    #             print(f"s: {s}")
+    #     l += 1
+    # if s > 800:
+    #     print(s-bidprice)
+    # elif s <-800:
+    #     print(s+askprice)
+    # else: print(s)
+    # print(f"sell: {dump}")
+    # print(f"buy: {pump_and_dump}")
 
-    i=200
-    s = 0
-    dump = []
-    pump_and_dump = []
-    while i <4650:
-        bid_price = df[i:i+1]["bid_price"]
-        print(f"bid_price: {bid_price}")
-        ask_price = df[i:i+1]["ask_price"]
-        print(f"ask_price: {ask_price}")
-        detect = MD.detect_pump_and_dump(i - 200, i)
-        if detect == "dump":
-            dump.append(i)
-            print(f"Detected dumping state at the following times!")
-        if detect == "pump and dump":
-            pump_and_dump.append(i)
-            print(f"Detected pumping and dumping state at the following times!")
-        if len(dump) > 0:
-            pump_and_dump = []
-            if len(dump) == 3:
-                s += bid_price
-                print(f"s: {s}")
-        if len(pump_and_dump) > 0:
-            dump = []
-            if len(pump_and_dump) == 3:
-                s -= ask_price
-                print(f"s: {s}")
-        i+=1
+    for i in range(1,31):
+        if i<10:
+            df_date = df[df.index.strftime("%Y-%m-%d").isin(["2020-09-0"+str(i)])]
+        if 10<=i<20:
+            df_date = df[df.index.strftime("%Y-%m-%d").isin(["2020-09-1"+str(i%10)])]
+        if 20<=i<30:
+            df_date = df[df.index.strftime("%Y-%m-%d").isin(["2020-09-2"+str(i%10)])]
+        if 30<=i:
+            df_date = df[df.index.strftime("%Y-%m-%d").isin(["2020-09-3"+str(i%10)])]
+
+        MD = ManipulationDetection(df_date)
+        l=200
+        s = 0
+        check = []
+        dump = []
+        pump_and_dump = []
+        while l <len(df_date):
+            bid_price = df_date[l-200:l]["bid_price"]
+            ask_price = df_date[l-200:l]["ask_price"]
+            detect = MD.detect_pump_and_dump(l - 200, l)
+            if detect == "dump":
+                check.append("dump")
+                print(f"Detected dumping state at the following times!")
+                if check[-3:] == ["dump", "dump", "dump"] and abs(len(dump) + 1 - len(pump_and_dump) < 2):
+                    bidprice = bid_price[-1]
+                    print(f"bid_price: {bidprice}")
+                    dump.append(bidprice)
+                    print(f"dump : {dump}")
+                    print(f"pump and dump: {pump_and_dump}")
+                    s += bidprice
+                    print(f"s: {s}")
+            if detect == "pump and dump":
+                check.append("pump and dump")
+                print(f"Detected pumping and dumping state at the following times!")
+                if check[-3:] == ["pump and dump", "pump and dump", "pump and dump"] and abs(
+                        len(pump_and_dump) + 1 - len(dump)) < 2:
+                    askprice = ask_price[-1]
+                    print(f"ask_price: {askprice}")
+                    pump_and_dump.append(askprice)
+                    print(f"dump : {dump}")
+                    print(f"pump and dump: {pump_and_dump}")
+                    s -= askprice
+                    print(f"s: {s}")
+            l+=1
+        if s > 800:
+            index.append(s-bidprice)
+        elif s <-800:
+            index.append(s+askprice)
+        else: index.append(s)
+    for n in range(0,30):
+        print(f"Ngay {n+1}/09/2020: {index[n]}")
+    print(index)
     #
     # print(pump_and_dump)
     # plt.plot(df[0:8000]['ask_price'])
     # plt.show()
 
     # print(MD.detect_bump_and_dump(232,432))
-    # print(tabulate(df[4650:4651], tablefmt='pipe', headers='keys'))
+    # print(tabulate(df[-7297:-3000], tablefmt='pipe', headers='keys'))
     # print(tabulate(df[200:555], tablefmt='pipe', headers='keys'))
     # print(tabulate(df[555:755], tablefmt='pipe', headers='keys'))
